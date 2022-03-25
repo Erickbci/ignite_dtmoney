8c17d551764ad1c0fc6b2f45c9bf5f880b1e71f9
Erickbci <batista.erick07@hotmail.com>
2022-03-25 00:01:35 -0300

    Fonte adicionada

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <div className="App">
      <h1>Hello Wordl</h1>
+     <GlobalStyle />
     </div>
   );
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
-const GlobalStyle = createGlobalStyle`
+export const GlobalStyle = createGlobalStyle`
     :root {
         --red: #E52E4D;
         --blue: #5429CC;
@@ -32,6 +32,16 @@ const GlobalStyle = createGlobalStyle`
     body {
         background: var(--background);
         -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, textarea, button {
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+    }
+
+    h1, h2, h3, h4, h5, h6, strong {
+        font-family: 'Poppins', sans-serif;
+        font-weight: 600;
     }
 
     button {
