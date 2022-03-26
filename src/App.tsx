134bb0c3936871bc7ef98487a7f17a2c09649fc8
Erickbci <batista.erick07@hotmail.com>
2022-03-26 12:35:03 -0300

    Configurando Miragejs

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,27 @@ import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
+import { createServer } from 'miragejs'
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+})
+
 export function App() {
   return (
     <>
