0681a4484a979a1bd28081cf2d5267365fb3295c
Erickbci <batista.erick07@hotmail.com>
2022-03-27 11:19:54 -0300

    Criando botões de Tipo (entrada e saída)

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal';
-import { Container } from './styles';
+import { Container, TransactionTypeContainer } from './styles';
 
+import incomeImg from '../../assets/income.svg' 
+import outcomeImg from '../../assets/outcome.svg' 
 import closeImg from '../../assets/close.svg'
 
 interface NewTransactionModalProps {
@@ -35,6 +37,22 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
         type='number'
         placeholder='Valor'
         />
+
+        <TransactionTypeContainer>
+      	  <button
+            type='button'
+          >
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+
+      	  <button
+            type='button'
+          >
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </button>
+        </TransactionTypeContainer>
 
         <input
         placeholder='Categoria'
