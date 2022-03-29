9a0a839223a3d5777edbbb46e5bf77264fc44999
Erickbci <batista.erick07@hotmail.com>
2022-03-28 22:44:07 -0300

    Funcionamento dos Botões

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,9 +1,10 @@
 import Modal from 'react-modal';
-import { Container, TransactionTypeContainer } from './styles';
+import { Container, TransactionTypeContainer, RadioBox } from './styles';
 
 import incomeImg from '../../assets/income.svg' 
 import outcomeImg from '../../assets/outcome.svg' 
 import closeImg from '../../assets/close.svg'
+import { useState } from 'react';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -11,6 +12,9 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+
+  const [type, setType] = useState('deposit')
+
   return (
     <Modal 
     isOpen={isOpen}
@@ -39,19 +43,23 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
         />
 
         <TransactionTypeContainer>
-      	  <button
+      	  <RadioBox
             type='button'
+            onClick={() => {setType('deposit')}}
+            isActive={type === 'deposit'}
           >
             <img src={incomeImg} alt="Entrada" />
             <span>Entrada</span>
-          </button>
+          </RadioBox>
 
-      	  <button
+      	  <RadioBox
             type='button'
+            onClick={() => {setType('withdraw')}}
+            isActive={type === 'withdraw'}
           >
             <img src={outcomeImg} alt="Saída" />
             <span>Saída</span>
-          </button>
+          </RadioBox>
         </TransactionTypeContainer>
 
         <input
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -55,13 +55,18 @@ export const TransactionTypeContainer = styled.div `
     display: grid;
     grid-template-columns: 1fr 1fr;
     gap: 0.5rem;
+`
 
-    button {
+interface RadioBoxProps {
+    isActive: boolean;
+}
+
+export const RadioBox = styled.button<RadioBoxProps>`
         height: 4rem;
         border: 1px solid #d7d7d7;
         border-radius: 0.25rem;
 
-        background: transparent;
+        background: ${(props) => props.isActive ? '#eee' : 'transparent'};
 
         display: flex;
         align-items: center;
@@ -84,5 +89,4 @@ export const TransactionTypeContainer = styled.div `
             font-size: 1rem;
             color: var(--text-title);
         }
-    }
 `
